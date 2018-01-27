d974ccb991536187ad2d916d400db9790f35e53e
Michael <ferrellmichael7@gmail.com>
2018-01-27 11:30:45 -0800

    commit chartiq

--- src/js/components/Dashboard/Overview/index.js
+++ src/js/components/Dashboard/Overview/index.js
@@ -1,11 +1,46 @@
-import React from "react"
+import React, { Component } from "react"
 import ContentComponent from "./../ContentComponent"
+import ChartistGraph from 'react-chartist';
 
-export default props => (
-  <ContentComponent
-    header="Overview"
-    subHeader="Manage your assets at a glance."
-  >
-    <pre>{JSON.stringify(props, null, 2)}</pre>
-  </ContentComponent>
-)
+const simpleLineChartData = {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  series: [
+    [12, 9, 7, 8, 5],
+    [2, 1, 3.5, 7, 3],
+    [1, 3, 4, 5, 6]
+  ]
+}
+
+export default class extends Component {
+  componentWillMount() {
+    const { fetchWallets, fetchCoins, wallets, coins } = this.props
+    if (!wallets.fetched) {
+      fetchWallets()
+    }
+    if (!coins.fetching_coins && !coins.fetched) {
+      fetchCoins()
+    }
+  }
+	render() {
+	const { coins, wallets } = this.props
+	return coins.fetched && wallets.fetched  // make sure app state contains what i need
+		? (
+		  <ContentComponent
+		    header="Overview"
+		    subHeader="Manage your assets at a glance."
+		  >
+		  <ChartistGraph data={simpleLineChartData} type={'Line'} />
+			</ContentComponent>
+		)
+		: <div>hi</div>
+	}
+}
+
+// export default props => (
+//   <ContentComponent
+//     header="Overview"
+//     subHeader="Manage your assets at a glance."
+//   >
+//     <pre>{JSON.stringify(props, null, 2)}</pre>
+//   </ContentComponent>
+// )
