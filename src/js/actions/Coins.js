fca2b6c95f236c42e43f944e04f381bdc0c50a10
mac <mac.gainor@gmail.com>
2018-01-31 20:25:19 -0800

    added time series fetch

--- src/js/actions/Coins.js
+++ src/js/actions/Coins.js
@@ -3,6 +3,9 @@ import axios from "axios"
 export const REQUEST_COINS = "REQUEST_COINS"
 export const RECEIVE_COINS = "RECEIVE_COINS"
 export const RECEIVE_COINS_ERROR = "RECEIVE_COINS_ERROR"
+export const REQUEST_TIME_SERIES = "REQUEST_TIME_SERIES"
+export const RECEIVE_TIME_SERIES = "RECEIVE_TIME_SERIES"
+export const RECEIVE_TIME_SERIES_ERROR = "RECEIVE_TIME_SERIES_ERROR"
 
 const requestCoins = () => ({
   type: REQUEST_COINS
@@ -15,7 +18,7 @@ const receiveCoins = response => ({
 
 const receiveCoinsError = payload => ({
   type: RECEIVE_COINS_ERROR,
-  payload: payload
+  payload
 })
 
 export const fetchCoins = payload => {
@@ -25,5 +28,31 @@ export const fetchCoins = payload => {
       .get("https://api.coinmarketcap.com/v1/ticker/")
       .then(response => dispatch(receiveCoins(response.data)))
       .catch(error => dispatch(receiveCoinsError(error)))
+  }
+}
+
+export const requestTimeSeries = () => ({
+  type: REQUEST_TIME_SERIES
+})
+
+export const receiveTimeSeries = payload => ({
+  type: RECEIVE_TIME_SERIES,
+  response
+})
+
+export const receiveTimeSeriesError = payload => ({
+  type: RECEIVE_TIME_SERIES_ERROR,
+  payload
+})
+
+export const fetchTimeSeries = () => {
+  return dispatch => {
+    dispatch(requestTimeSeries)
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=60&aggregate=3&e=CCCAGG"
+      )
+      .then(response => dispatch(receiveTimeSeries(response.data)))
+      .catch(error => dispatch(receiveTimeSeriesError(error)))
   }
 }
