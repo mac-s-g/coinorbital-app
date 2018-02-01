d9a5d62565b3ea14054a747d177013f6f9ae111b
Michael <ferrellmichael7@gmail.com>
2018-01-31 20:23:07 -0800

    move charts into separate components

---
+++ src/js/components/Charts/Pie/Donut.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react"
+import ChartistGraph from "react-chartist"
+import ChartistLegend from "chartist-plugin-legend"
+import Styled from "styled-components"
+
+import round from "./../../../helpers/round"
+import formatNumberForDisplay from "./../../../helpers/formatNumberForDisplay"
+import { theme } from "./../../../constants"
+
+import {
+	calculateWalletQuantity,
+	calculateWalletValue
+} from "./../../../helpers/walletMetrics"
+
+const DonutComponent = Styled.div`
+  & .ct-chart {
+    position: relative;
+    margin-bottom: 15px;
+  }
+  & .ct-chart-donut .ct-series-a .ct-slice-donut-solid {
+    fill: ${props => props.themeColors.blue};
+  }
+  .ct-chart-donut .ct-series-b .ct-slice-donut-solid {
+    fill: #E1E9EB;
+  }
+  & .ct-chart-donut .ct-label {
+    fill: #333;
+    color: #333;
+    font-size: 1.4rem;
+    dominant-baseline: central;
+    text-anchor: middle;
+  }
+  & .ct-legend {
+    position: relative;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+  & .ct-legend li {
+    color: rgba(0, 0, 0, 0.5);
+    display: inline-block;
+    font-size: 1.2rem;
+    list-style: none;
+    margin: 0;
+    margin-bottom: 20px;
+  }
+  & .ct-legend li:hover {
+    color: rgba(0, 0, 0, 0.85);
+  }
+  & .ct-legend li.inactive {
+    opacity: .6;
+  }
+`
+
+const centerChartLabel = {
+  draw(context) {
+    if (context.type === "label") {
+      context.element.attr({
+        dx: context.element.root().width() / 2,
+        dy: context.element.root().height() / 2
+      })
+    }
+  }
+}
+
+export default class extends Component {
+	getTotalValue = () => {
+		const { coins, wallets } = this.props
+		let totalValue = Object.keys(wallets.by_name)
+			.map(key => {
+				return calculateWalletValue(
+					wallets.by_name[key],
+					coins.by_symbol[wallets.by_name[key].symbol].price_usd
+				)
+			})
+			.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+		return round(totalValue, 2)
+	}
+
+	buildChart = () => {
+		const { coins, wallets } = this.props
+		const totalValue = this.getTotalValue()
+		return Object.keys(wallets.by_name).map(key => {
+			let wallet = wallets.by_name[key]
+			let value = calculateWalletValue(
+				wallet,
+				coins.by_symbol[wallet.symbol].price_usd
+			)
+			let difference = totalValue - value
+			let data = {
+				series: [round(value, 2), round(difference, 2)],
+				labels: [coins.by_symbol[wallet.symbol].name]
+			}
+			let options = {
+				width: "300px",
+				height: "300px",
+				total: 100,
+				donut: true,
+				donutWidth: 10,
+				donutSolid: true,
+				plugins: [ChartistLegend()]
+			}
+			options.total = totalValue
+			options.labelInterpolationFnc = val => {
+				return (
+					"$" +
+					formatNumberForDisplay(round(value), 2) +
+					" / $" +
+					formatNumberForDisplay(round(totalValue), 2)
+				)
+			}
+
+			options.labelInterpolationFnc = val => {
+				return (
+					"$" +
+					formatNumberForDisplay(round(value), 2) +
+					" / $" +
+					formatNumberForDisplay(round(totalValue), 2)
+				)
+			}
+
+			return (
+				<ChartistGraph
+					key={key}
+					data={data}
+					type={"Pie"}
+					options={options}
+					listener={centerChartLabel}
+				/>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<DonutComponent themeColors={theme.colors}>
+				{this.buildChart()}
+			</DonutComponent>
+		)
+	}
+}
---
+++ src/js/components/Charts/Pie/index.js
@@ -0,0 +1,157 @@
+import React, { Component } from "react"
+import ChartistGraph from "react-chartist"
+import ChartistLegend from "chartist-plugin-legend"
+import Styled from "styled-components"
+
+import round from "./../../../helpers/round"
+import { theme } from "./../../../constants"
+
+import {
+	calculateWalletQuantity,
+	calculateWalletValue
+} from "./../../../helpers/walletMetrics"
+
+const PieComponent = Styled.div`
+  & .ct-chart-pie {
+    margin-top: 30px;
+  }
+  & .ct-series-a .ct-slice-pie {
+    fill: ${props => props.themeColors.blue};
+  }
+  & .ct-series-b .ct-slice-pie {
+    fill: ${props => props.themeColors.red};
+  }
+  & .ct-series-c .ct-slice-pie {
+    fill: ${props => props.themeColors.green};
+  }
+  & .ct-series-d .ct-slice-pie {
+    fill: ${props => props.themeColors.gold};
+  }
+  & .ct-label {
+    fill: #fff;
+    color: #fff;
+    font-size: 1.15rem;
+  }
+  & .ct-chart {
+    position: relative;
+  }
+  & .ct-legend {
+    padding: 0;
+  }
+  & .ct-legend li {
+    color: rgba(0, 0, 0, 0.6);
+    position: relative;
+    padding-left: 16px;
+    margin-bottom: 3px;
+    margin-right: 18px;
+    list-style: none;
+    display: block;
+    float: left;
+    cursor: pointer;
+  }
+  & .ct-legend li:hover {
+    color: rgba(0, 0, 0, 0.85);
+  }
+  & .ct-legend li.inactive {
+    opacity: .6;
+  }
+  & .ct-legend li:before {
+    width: 12px;
+    height: 12px;
+    position: absolute;
+    top: 4px;
+    left: 0;
+    content: '';
+    border: 3px solid transparent;
+    border-radius: 2px;
+  }
+  & .ct-legend li:before {
+    background: #333;
+  }
+  & .ct-legend .ct-series-0:before {
+    background: ${props => props.themeColors.blue};
+  }
+  & .ct-legend .ct-series-1:before {
+    background: ${props => props.themeColors.red};
+  }
+  & .ct-legend .ct-series-2:before {
+    background: ${props => props.themeColors.green};
+  }
+  & .ct-legend .ct-series-3:before {
+    background: ${props => props.themeColors.gold};
+  }
+`
+
+export default class extends Component {
+	getTotalValue = () => {
+		const { coins, wallets } = this.props
+		let totalValue = Object.keys(wallets.by_name)
+			.map(key => {
+				return calculateWalletValue(
+					wallets.by_name[key],
+					coins.by_symbol[wallets.by_name[key].symbol].price_usd
+				)
+			})
+			.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+		return round(totalValue, 2)
+	}
+
+	buildChart = () => {
+		const { coins, wallets } = this.props
+		const totalValue = this.getTotalValue()
+		let pieChartOptions = {
+			stretch: true,
+			width: "400px",
+			height: "400px",
+			plugins: [ChartistLegend()]
+		}
+
+		let pieChartSeries = Object.keys(wallets.by_name).map(key => ({
+			name: wallets.by_name[key].symbol,
+			value: round(
+				calculateWalletValue(
+					wallets.by_name[key],
+					coins.by_symbol[wallets.by_name[key].symbol].price_usd
+				),
+				2
+			)
+		}))
+
+		let pieChartData = {
+			series: pieChartSeries
+		}
+
+		let pieChartResponsiveOptions = [
+			[
+				"screen and (min-width: 600px)",
+				{
+					labelInterpolationFnc(value, index) {
+						////////// fix soon
+						return "$" + value
+						let percentage = value / totalValue * 100 + "%"
+						let label = parseInt(percentage) > 3 ? "$" + value : null
+						return label
+					}
+				}
+			]
+		]
+
+		return (
+			<ChartistGraph
+				data={pieChartData}
+				type={"Pie"}
+				options={pieChartOptions}
+				responsiveOptions={pieChartResponsiveOptions}
+			/>
+		)
+	}
+
+	render() {
+		return (
+			<PieComponent themeColors={theme.colors}>
+				{this.buildChart()}
+			</PieComponent>
+		)
+	}
+}
